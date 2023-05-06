0a855f67bcb9b6e0bf5e3e62805a9810d2a7b2e7
Ali Sami <a.sami@buildmart.net>
2023-05-06 21:43:43 +0300

    Validate Api Services

---
+++ Gateway/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use("/", func(c *fiber.Ctx) error {
+
+		URI := c.Context().URI()
+
+		uriSplit := strings.Split(string(URI.FullURI()), "/")
+
+		// method := c.Context().Method()
+
+		// headers := c.Context().Request.Header
+
+		// body = c.Context().Request.Body()
+
+		fmt.Println(uriSplit[3])
+
+		type service struct {
+			Name   string `json:"name"`
+			Domain string `json:"domain"`
+		}
+
+		var services []service
+		services = append(services, service{Name: "home", Domain: "http://127:0.0.1:6000"})
+		services = append(services, service{Name: "admins", Domain: "http://127:0.0.1:5000"})
+		services = append(services, service{Name: "users", Domain: "http://127:0.0.1:5050"})
+		services = append(services, service{Name: "cart", Domain: "http://127:0.0.1:6060"})
+		services = append(services, service{Name: "services", Domain: "http://127:0.0.1:7000"})
+		services = append(services, service{Name: "products", Domain: "http://127:0.0.1:7070"})
+		services = append(services, service{Name: "order-management", Domain: "http://127:0.0.1:8000"})
+
+		var requestedService service
+
+		for _, item := range services {
+			if item.Name == uriSplit[3] {
+				requestedService = item
+			}
+		}
+
+		if requestedService.Name == "" {
+			return c.Status(fiber.StatusBadRequest).JSON("service not available")
+		}
+
+		fmt.Println(requestedService)
+		return c.JSON(requestedService)
+	})
+
+	app.Listen(":3000")
+}
